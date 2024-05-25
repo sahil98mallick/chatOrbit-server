d0b0fceccabaac611681026e68f8c403211d6cf8
sahil98mallick <sahil98mallick.com>
2024-05-25 12:49:45 +0530

    Swagger Updated

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerSpec, {
-    // customCss:
-    //   ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 5px; width: 100%; }",
     customCssUrl: CSS_URL,
   })
 );
